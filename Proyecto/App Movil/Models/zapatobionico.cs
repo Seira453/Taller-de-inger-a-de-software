bd5416e413631b9ae69aed62ebbdf8c625c7a117
Seira453 <brendacoralsr@gmail.com>
2026-04-09 17:44:41 -0600

    Update zapatobionico.cs

--- Proyecto/App Movil/Models/zapatobionico.cs
+++ Proyecto/App Movil/Models/zapatobionico.cs
@@ -1,1 +1,10 @@
-
+namespace AppMovil.Models
+{
+    public class ZapatoBionico
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int NivelBateria { get; set; }
+        public bool Estado { get; set; }
+    }
+}
